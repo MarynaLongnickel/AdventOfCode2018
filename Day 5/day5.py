7ce9afac27a85e10629b3a2e12ff0a85c3fdbdf1
Maryna Longnickel <37321032+MarynaLongnickel@users.noreply.github.com>
2018-12-05 10:21:35 -0500

    Create day5.py

---
+++ Day 5/day5.py
@@ -0,0 +1,42 @@
+# --- Day 5: Alchemical Reduction ---
+
+from urllib.request import urlopen
+import string
+
+data = urlopen('https://raw.githubusercontent.com/MarynaLongnickel/AdventOfCode2018/master/Day%205/day5.txt')
+
+for line in data: poly = line.decode()
+poly = poly[:-1]
+  
+l = len(poly)
+alphabet = string.ascii_lowercase
+
+while True:
+  for a in alphabet:
+    poly = poly.replace(a + a.upper(), '')
+    poly = poly.replace(a.upper() + a, '')
+  if len(poly) == l:
+    print(len(poly))
+    break
+  l = len(poly)
+  
+# ---------------------- PART 2 ------------------------
+
+shortest = 100000  
+
+for a in alphabet:
+  poly2 = poly2.replace(a, '')
+  poly2 = poly2.replace(a.upper(), '')
+  
+  l = len(poly2)
+
+  while True:
+    for a in alphabet:
+      poly2 = poly2.replace(a + a.upper(), '')
+      poly2 = poly2.replace(a.upper() + a, '')
+    if len(poly2) == l:
+      if len(poly2) < shortest:
+        shortest = len(poly2)
+      break
+    l = len(poly2)
+print(shortest)
