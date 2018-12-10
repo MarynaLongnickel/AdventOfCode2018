0e7e040a84e96e41b92c06218def0eda336e9d68
Maryna Longnickel <37321032+MarynaLongnickel@users.noreply.github.com>
2018-12-10 14:00:45 -0500

    Update day10.py

--- Day 10/day10.py
+++ Day 10/day10.py
@@ -6,10 +6,8 @@ import re
 data = urlopen('https://raw.githubusercontent.com/MarynaLongnickel/AdventOfCode2018/master/Day%2010/day10.txt').read().decode()
 nums = np.reshape([int(i) for i in re.findall('-?\d+', data)], (-1,4))
 
-dis = 200000
-
-s = 0
-sky = [[0 for i in range(80)] for j in range(20)]
+w, h, dis = 80, 20, 200000
+sky = [[0 for i in range(w)] for j in range(h)]
 
 for i in range(11000):
     
@@ -17,11 +15,9 @@ for i in range(11000):
   miny, maxy = min([i[1] for i in nums]), max([i[1] for i in nums])
   
   mx, my = abs(maxx - minx), abs(maxy - miny)
-    
+  
   if min(mx, my) > dis:
     print(i-1)
-    imshow(skyy)
-    plt.show()
     break
   else: dis = min(mx, my)
     
@@ -32,5 +28,7 @@ for i in range(11000):
     n[0] += n[2]
     n[1] += n[3]
     
-    if mx < 150 and n[0] - minx < len(sky[0]) and n[1] - miny < len(sky):
+    if mx < 150 and n[0] - minx < w and n[1] - miny < h:
       sky[n[1] - miny][n[0] - minx] = 1
+        
+imshow(skyy);
